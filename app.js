b956661f88f68651ff4cee4d19cea60e13496799
Snehal5101 <manesnehal357@gmail.com>
2021-10-20 11:52:00 +0530

    js intro

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("this is external js");
+
+function studentdata() {
+    var name= "Ram";
+    var b= 5;
+    var address="xyz";
+
+    console.log("Student Name");
+    console.log(name);
+
+    console.log("Student Class");
+    console.log(b);
+
+    console.log("Student Address");
+    console.log(address);
+
+}
+studentdata();
